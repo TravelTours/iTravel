2dfec3962de4fd4b00bad78ff97031df0e4582f6
shao1-crypto <77039804+shao1-crypto@users.noreply.github.com>
2021-04-29 22:34:30 -0700

    Add files via upload

---
+++ iTravel/iTravel/FeedViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FeedViewController.swift
+//  iTravel
+//
+//  Created by Shijia Hao on 4/29/21.
+//  Copyright © 2021 shijiah2012@gmail.com. All rights reserved.
+//
+
+import UIKit
+import MessageInputBar
+
+class FeedViewController: ViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    let commentBar = MessageInputBar()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
